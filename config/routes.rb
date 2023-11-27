87c02cc10e1c86dd35b9f02d343eeb51a2fc3347
yuni-tsuki <huzukihayama@gmail.com>
2023-11-27 21:57:06 +0900

    7.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "articles#index"
+
   get "/articles", to: "articles#index"
+  get "/articles/:id", to: "articles#show"
 end
