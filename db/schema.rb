56b9d69621cac4f6b5eda18f8ab6cc0c319c06d4
yuni-tsuki <huzukihayama@gmail.com>
2023-12-06 16:04:03 +0900

    add counter and status of articles and comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_04_011205) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_04_011943) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
